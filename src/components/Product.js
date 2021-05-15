4e4c756116ed5b930985d8f39a32342af927b649
Jerry <jerrsiphavong@icloud.com>
2021-05-15 16:35:04 -0400

    try

--- src/components/Product.js
+++ src/components/Product.js
@@ -13,11 +13,7 @@ const Product = ({ imageUrl, description, price, name, productId }) => {
 
         <p className="info__price">${price}</p>
 
-        {/* <Link to={`api/products/${productId}`} className="info__button"> */}
-        <Link
-          href="https://theillissionproject.herokuapp.com/api/products/${productId"
-          className="info__button"
-        >
+        <Link to={`/products/${productId}`} className="info__button">
           View
         </Link>
       </div>
